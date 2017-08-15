3cdf0e9a63a7c726dbce7c92ba85b87d8bd7894a
Anatolijus <join.eu.pn@gmail.com>
2017-08-15 12:51:42 +0300

    ps0 RandomHello basic

--- src/ps0/RandomHello.java
+++ src/ps0/RandomHello.java
@@ -1,9 +1,23 @@
 package ps0;
 
+/**
+ * RandomHello selects a random greeting to display to the user.
+ */
 public class RandomHello {
 
+    /**
+     * @effects uses a RandomHello object to print a random greeting to the console
+     */
     public static void main(String args) {
+        RandomHello randomHello = new RandomHello();
+        System.out.println(randomHello.sayHello());
+    }
 
+    /**
+     * @return a random greeting from a list of five different greetings.
+     */
+    public String sayHello() {
+        return "";
     }
 
 }
