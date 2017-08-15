6f9d89a927c8964f263752e92b3beeb6aa8f6f7d
Anatolijus <join.eu.pn@gmail.com>
2017-08-15 13:21:02 +0300

    ps0 RandomHello implementation

--- src/ps0/RandomHello.java
+++ src/ps0/RandomHello.java
@@ -20,7 +20,13 @@ public class RandomHello {
      */
     public String sayHello() {
         Random randomGenerator = new Random();
-        return "";
+        String[] greetings = new String[5];
+        greetings[0] = "Hello World";
+        greetings[1] = "Hola Mundo";
+        greetings[2] = "Bonjour Monde";
+        greetings[3] = "Hallo Welt";
+        greetings[4] = "Ciao Mondo";
+        return greetings[randomGenerator.nextInt(5)];
     }
 
 }
